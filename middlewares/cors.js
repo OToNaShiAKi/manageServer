3840aeb7d600f0d5d806916af9be2fac29a44e1c
寿鹏鹏 <shoupengpeng@xiaomi.com>
2020-11-16 16:16:31 +0800

    重写项目

--- middlewares/cors.js
+++
@@ -1,9 +0,0 @@
-const allowCrossOrigin = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 'http://59.110.40.182');
-    res.header("Access-Control-Allow-Methods", 'GET,POST');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header("Access-Control-Allow-Headers", 'Content-Type');
-    next();
-}
-
-export default allowCrossOrigin
